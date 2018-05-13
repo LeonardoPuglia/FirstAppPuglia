c32d403f169d87736dd2a736fa38df324e776b82
Leonardo Puglia <leo.puglia@hotmail.com>
2018-05-13 17:03:06 -0300

    Finalizando o App de conteúdo estático

---
+++ App_Xamarin/App_Xamarin/App_Xamarin/Master/Menu.xaml.cs
@@ -0,0 +1,35 @@
+﻿using App_Xamarin.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Xamarin.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoToPerfilPage(object sender , EventArgs args)
+        {
+            Detail = new NavigationPage(new Perfil());
+            IsPresented = false;
+        }
+        private void GotToAboutXamarin(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage( new XamarinAbout() );
+            IsPresented = false;
+        }
+
+
+
+    }
+}
